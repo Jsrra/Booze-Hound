cb02cc51e3a477b9a2595ee1a8c48fcf0fc75e18
ikaera <ikaera@gmail.com>
2023-01-24 12:22:43 -0500

    use getCurrentPosition method and  add Leaflet  map to the page.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,28 @@
 
 // Leaflet Map script
 
-map
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(function (myPosition) {
+
+    const lat = myPosition.coords.latitude;
+    const lon = myPosition.coords.longitude;
+    // console.log(lat, lon);
+
+    const currentCords = [lat, lon];
+
+    const map = L.map('map').setView(currentCords, 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(currentCords).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
+
+  }, function () {
+    alert('It cannot find the current location.');
+  });
 
 // console.log('test');
